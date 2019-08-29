bdcd6f18ee197a145d316b35c3fced3ea598980d
RelaxHuang <relax0914@gmail.com>
2019-08-29 11:00:38 +0800

    Create GrabberComics.js

---
+++ GrabberComics.js
@@ -0,0 +1,9 @@
+var header = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+  'Referer': 'https://kkpmh.com/'
+};
+
+var imageTags = document.getElementsByTagName('img');
+while(i=0; i<imageTags.length; i++) {
+  console.log(imageTags[i]);
+}
